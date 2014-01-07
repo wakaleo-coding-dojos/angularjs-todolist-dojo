8d8982a97bc82d4fa8a2a0450c4bbfc5d2257535
John Smart <john.smart@wakaleo.com>
2014-01-08 07:47:10 +1100

    Added the menu bar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,26 @@
 'use strict';
 
 angular.module('thucydidesUiApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/dashboard', {
+                templateUrl: 'views/dashboard.html',
+                controller: 'DashboardCtrl'
+            })
+            .when('/outcomes/:outcomeId', {
+                templateUrl: 'outcomes/outcomes.tpl.html',
+                controller: 'OutcomeCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
